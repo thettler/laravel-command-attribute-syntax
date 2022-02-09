1d5c9e1f82f09617104a00513cff6ce50ec13692
thettler <thettler@users.noreply.github.com>
2022-02-09 23:20:22 +0000

    Fix styling

--- src/Casts/EnumCast.php
+++ src/Casts/EnumCast.php
@@ -8,7 +8,7 @@ class EnumCast implements CastInterface
 {
     public static function match(string $typeName, mixed $value): bool
     {
-      return enum_exists($typeName);
+        return enum_exists($typeName);
     }
 
     public function cast(mixed $value, string $typeName): \UnitEnum
